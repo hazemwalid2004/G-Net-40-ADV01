982b3674bf281a8d3191af58e9d843ff47ab93e6
Hazem walid <walidf106@gmail.com>
2026-03-16 03:29:05 +0200

    Question6

---
+++ assAdvancedC#01/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assAdvancedC_01
+{
+    internal interface IRepository<T>
+    {
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(int id);
+    }
+}
