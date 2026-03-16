8ec97795b84b54dc87590091571bf2da0cb8ee94
Hazem walid <walidf106@gmail.com>
2026-03-16 02:59:20 +0200

    Qustion1

--- assAdvancedC#01/Program.cs
+++ assAdvancedC#01/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            #region Qustion1
+            //What is a generic class? Why use generics?
+            //uses type parameters that are replaced with actual types when you create an instance
+            //to avoid duplication and object class
+            #endregion
         }
     }
 }
