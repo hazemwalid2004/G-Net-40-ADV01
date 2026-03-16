75c98ea7109fe32bb5241953619d36ca204520cf
Hazem walid <walidf106@gmail.com>
2026-03-16 03:06:59 +0200

    Qustion2

--- assAdvancedC#01/Program.cs
+++ assAdvancedC#01/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace assAdvancedC_01
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace assAdvancedC_01
 {
     internal class Program
     {
@@ -9,6 +12,9 @@
             //What is a generic class? Why use generics?
             //uses type parameters that are replaced with actual types when you create an instance
             //to avoid duplication and object class
+            #endregion
+            #region Qustion2
+            // Write a generic class Container<T> with Add and Get methods.
             #endregion
         }
     }
